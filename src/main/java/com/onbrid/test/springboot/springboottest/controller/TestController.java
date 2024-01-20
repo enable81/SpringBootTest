4afd765c688a097ccc891686e43d6fed715eadd4
강명원 <enable81@gmail.com>
2024-01-20 15:54:52 +0900

    DB연결(DataSource) 설정 테스트

--- src/main/java/com/onbrid/test/springboot/springboottest/controller/TestController.java
+++ src/main/java/com/onbrid/test/springboot/springboottest/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.onbrid.test.springboot.springboottest.controller;
 
+import com.onbrid.test.springboot.springboottest.service.TestService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-public class LogController {
+@RequiredArgsConstructor
+public class TestController {
 
-    @GetMapping("/log")
-    public String log() {
+    // final로 생성된 변수는 롬복이(@RequiredArgsConstructor) 알아서 생성자를 만들어준다.
+    // 따라서 스프링에 생성자 주입방식으로 인젝션된다. @Autowired 해결 
+    final TestService testService;
+
+    @GetMapping("/test")
+    public String test() {
         // TRACE > DEBUG > INFO > WARN > ERROR
         log.trace("TRACE");
         log.debug("DEBUG");
