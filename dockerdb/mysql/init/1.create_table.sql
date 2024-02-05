40a7de75458b0bde57052985d2b7739aace905ac
강명원 <enable81@gmail.com>
2024-02-05 11:01:57 +0900

    환경설정 파일 단방향 암호화 처리

---
+++ dockerdb/mysql/init/1.create_table.sql
@@ -0,0 +1,11 @@
+create table AA_TEST
+(
+    id          varchar(20) not null
+        primary key,
+    name        varchar(50) not null,
+    email       varchar(50) null,
+    regist_date datetime    null
+);
+
+
+
